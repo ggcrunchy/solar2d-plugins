56e8660d7c139f4f8965941c44f6eb136907937b
ggcrunchy <steve@xibalbastudios.com>
2022-03-19 01:58:22 -0500

    Better capture test, now with multiple events and samplings

--- engine_testing/capture/main.lua
+++ engine_testing/capture/main.lua
@@ -22,36 +22,58 @@
 --
 -- [ MIT license: http://www.opensource.org/licenses/mit-license.php ]
 --
---[[
-for i = 1, 1000 do
-  local x, y, radius = math.random( 10, display.contentWidth - 10 ), math.random( 10, display.contentHeight - 10 ), math.random( 5, 15 )
-  local circle = display.newCircle( x, y, radius )
 
-  circle:setFillColor( math.random(), math.random(), math.random() )
-end
-]]
 local image = display.newImageRect( "Image1.jpg", display.viewableContentWidth, display.viewableContentHeight )
 
 image.x, image.y = display.contentCenterX, display.contentCenterY
 
-local captureTexture = graphics.newTexture{ type = "capture", width = 200, height = 200 }
+local captureTexture1 = graphics.newTexture{ type = "capture", width = 200, height = 200 }
+local captureTexture2 = graphics.newTexture{ type = "capture", width = 100, height = 150 }
 
-local captureEvent = captureTexture:newCaptureEvent( display.contentCenterX, display.contentCenterY )
-local circle = display.newCircle( captureEvent.x, captureEvent.y, 100 )
---circle.isVisible=false
-circle.yScale=-1
-local aa = display.newImageRect( captureTexture.filename, captureTexture.baseDir, 75, 75 )
+local captureEvent1 = captureTexture1:newCaptureEvent( display.contentCenterX, display.contentCenterY ) -- capture to texture #1...
+local circle = display.newCircle( captureEvent1.x, captureEvent1.y, 100 )
 
-aa.x, aa.y = display.contentCenterX + 150, display.contentCenterY
+circle.yScale = -1
 
-aa.strokeWidth = 3
+local image1 = display.newImageRect( captureTexture1.filename, captureTexture1.baseDir, 75, 75 ) -- ...sample it in one spot...
 
-aa:setStrokeColor(1, 0, 0)
-aa.yScale=-1
-local rr=display.newRect(captureEvent.x,captureEvent.y,200,200)
-rr:setFillColor(0,0)
-rr:setStrokeColor(0,1,0)
-rr.strokeWidth=2
+image1:setStrokeColor( 1, 0, 0 )
+
+image1.x, image1.y = display.contentCenterX + 150, display.contentCenterY
+image1.strokeWidth = 3
+image1.yScale = -1
+
+local image2 = display.newImageRect( captureTexture1.filename, captureTexture1.baseDir, 120, 120 ) -- ...and in another
+
+image2:setStrokeColor( 0, 1, 1 )
+
+image2.x, image2.y = image1.x, image1.y + 250
+image2.strokeWidth = 3
+
+local captureEvent2 = captureTexture2:newCaptureEvent( image2.x - 20, image2.y + 20 ) -- capture to texture #2 (picking up part of image2)...
+local image3 = display.newRoundedRect( display.contentCenterX - 200, display.contentCenterY - 100, 100, 100, 12 )
+
+image3:setStrokeColor( 1, 0, 1 )
+
+image3.fill = { type = "image", filename = captureTexture2.filename, baseDir = captureTexture2.baseDir } -- ...and sample it...
+image3.rotation = 35
+image3.strokeWidth = 3
+
+local outline = display.newRect( captureEvent1.x, captureEvent1.y, 200, 200 )
+
+outline:setFillColor( 0, 0 )
+outline:setStrokeColor( 0, 1, 0 )
+
+outline.strokeWidth = 2
+
+local captureEvent3 = captureTexture2:newCaptureEvent( display.contentCenterX - 100, 100 ) -- ... then later capture to it from somewhere else... 
+local image4 = display.newImageRect( captureTexture2.filename, captureTexture2.baseDir, 250, 150 ) -- ...and then sample that
+
+image4:setStrokeColor( 1, 1, 0 )
+
+image4.x, image4.y = display.contentCenterX, display.contentHeight - 150
+image4.strokeWidth = 3
+
 local back = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
 
 back.isVisible, back.isHitTestable = false, true
@@ -61,9 +83,10 @@ back:addEventListener( "touch", function( event )
     if event.phase == "began" then
       display.getCurrentStage():setFocus(event.target)
     end
-    captureEvent.x, captureEvent.y = event.x, event.y
+
+    captureEvent1.x, captureEvent1.y = event.x, event.y
     circle.x, circle.y = event.x, event.y
-    rr.x,rr.y=event.x,event.y
+    outline.x, outline.y = event.x, event.y
   else
     display.getCurrentStage():setFocus(nil)
   end
@@ -84,5 +107,24 @@ graphics.defineEffect{
   ]]
 }
 
-circle.fill = { type = "image", filename = captureTexture.filename, baseDir = captureTexture.baseDir }
+circle.fill = { type = "image", filename = captureTexture1.filename, baseDir = captureTexture1.baseDir }
 --circle.fill.effect = "filter.custom.wavy"
+
+-- For feature-testing on phones etc.:
+local ext = system.getInfo( "GL_EXTENSIONS" )
+local ok = ext:find( "GL_NV_framebuffer_blit" ) or ext:find( "GL_ANGLE_framebuffer_blit" )
+
+local obj1 = display.newCircle( display.contentCenterX - 30, display.contentCenterY, 10 )
+local obj2 = display.newCircle( display.contentCenterX + 30, display.contentCenterY, 10 )
+
+if system.getInfo( "gpuSupportsScaledCaptures" ) then
+  obj1:setFillColor( 0, 0, 1 )
+else
+  obj1:setFillColor( 0 )
+end
+
+if ok then
+  obj2:setFillColor( 1, 0, 0 )
+else
+  obj2:setFillColor( 0 )
+end
